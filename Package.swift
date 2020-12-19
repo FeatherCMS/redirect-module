fa33fb8f87663a5af315bc4b68fe6a2fe4751151
Tibor Bödecs <mail.tib@gmail.com>
2020-12-19 15:50:55 +0100

    FC update, fixed admin templates

--- Package.swift
+++ Package.swift
@@ -14,6 +14,14 @@ let package = Package(
         
         .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.0.0"),
         .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.2.0-beta"),
+        
+        /// core modules
+        .package(name: "system-module", url: "https://github.com/feather-modules/system", from: "1.0.0-beta"),
+        .package(name: "user-module", url: "https://github.com/feather-modules/user", from: "1.0.0-beta"),
+        .package(name: "api-module", url: "https://github.com/feather-modules/api", from: "1.0.0-beta"),
+        .package(name: "admin-module", url: "https://github.com/feather-modules/admin", from: "1.0.0-beta"),
+        .package(name: "frontend-module", url: "https://github.com/feather-modules/frontend", from: "1.0.0-beta"),
+        
     ],
     targets: [
         .target(name: "RedirectModule", dependencies: [
@@ -26,6 +34,13 @@ let package = Package(
             
             .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
             .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
+            
+            /// core modules
+            .product(name: "SystemModule", package: "system-module"),
+            .product(name: "UserModule", package: "user-module"),
+            .product(name: "ApiModule", package: "api-module"),
+            .product(name: "AdminModule", package: "admin-module"),
+            .product(name: "FrontendModule", package: "frontend-module"),
             
             .target(name: "RedirectModule"),
         ]),
--- Sources/Feather/main.swift
+++ Sources/Feather/main.swift
@@ -8,7 +8,17 @@
 import FeatherCore
 import FluentSQLiteDriver
 import LiquidLocalDriver
+
+import SystemModule
+import UserModule
+import ApiModule
+import AdminModule
+import FrontendModule
+
 import RedirectModule
+
+/// setup metadata delegate object
+Feather.metadataDelegate = FrontendMetadataDelegate()
 
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
@@ -19,5 +29,16 @@ try feather.configure(database: .sqlite(.file("db.sqlite")),
                       databaseId: .sqlite,
                       fileStorage: .local(publicUrl: Application.baseUrl, publicPath: Application.Paths.public, workDirectory: "assets"),
                       fileStorageId: .local,
-                      modules: [RedirectBuilder()])
+                      modules: [
+                        SystemBuilder(),
+                        UserBuilder(),
+                        ApiBuilder(),
+                        AdminBuilder(),
+                        FrontendBuilder(),
+                        
+                        RedirectBuilder()
+                      ])
+if feather.app.isDebug {
+    try feather.reset(resourcesOnly: true)
+}
 try feather.start()
