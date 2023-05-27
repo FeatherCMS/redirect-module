6907ca9ea8524756fc5ebb55f59b5cff7e3d2390
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2023-05-27 18:50:40 -0500

    re-enables remote SP

--- Package.swift
+++ Package.swift
@@ -10,10 +10,13 @@ let package = Package(
         .library(name: "RedirectModule", targets: ["RedirectModule"]),
     ],
     dependencies: [
-		.package(path: "../feather-core"),
-		.package(path: "../redirect-objects"),
-//        .package(url: "https://github.com/FeatherCMS/feather-core", .branch("dev")),
-//        .package(url: "https://github.com/FeatherCMS/redirect-objects", .branch("main")),
+//		.package(path: "../feather-core"),
+//		.package(path: "../redirect-objects"),
+        .package(url: "https://github.com/Rando-Coderissian/feather-core", .branch("test-refactored-modules")),
+        .package(url: "https://github.com/Rando-Coderissian/redirect-objects", .branch("test-refactor-modules")),
+		
+//        .package(url: "https://github.com/feathercms/feather-core", .branch("dev")),
+//        .package(url: "https://github.com/feathercms/redirect-objects", .branch("main")),
     ],
     targets: [
         .target(name: "RedirectModule", dependencies: [
